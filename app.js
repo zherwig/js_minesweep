555ba75eabfe3eb0b8657bea92569876030c79bd
Zjef Herwig <zherwig@hotmail.com>
2020-07-28 07:35:37 +0300

    Basic adjacent bomb checking

--- app.js
+++ app.js
@@ -20,13 +20,52 @@ document.addEventListener("DOMContentLoaded", () => {
             square.classList.add(shuffleArray[i])
             grid.appendChild(square)
             squares.push(square)
+
+            //normal click
+            square.addEventListener('click', function(e) {
+                click(square)
+            })
+
+        }
+        //add numbers
+        for(let i = 0; i < squares.length; i++){
+            total = 0
+            //checking for left and right edges as these onlt check ajacent squares on one side
+            const isLeftEdge = (i % width === 0)
+            const isRightEdge = (i % width-1 === 0)
+
+            if (squares[i].classList.contains('valid')){
+                if ( i > 0 && !isLeftEdge && squares[i-1].classList.contains('bomb')) total++
+                if ( i > 9 && !isRightEdge && squares[i+1 -width].classList.contains('bomb')) total++
+                if ( i > 10 && squares[i -width].classList.contains('bomb')) total++
+                if ( i > 11 && !isLeftEdge && squares[i -1 -width].classList.contains('bomb')) total++
+                if ( i < 98 && !isRightEdge && squares[i + 1].classList.contains('bomb')) total++
+                if ( i < 90 && !isLeftEdge && squares[i - 1 + width].classList.contains('bomb')) total++
+                if ( i < 88 && !isRightEdge && squares[i + 1 + width].classList.contains('bomb')) total++
+                if ( i < 89 && squares[i + width].classList.contains('bomb')) total++
+                squares[i].setAttribute('data', total)
+            }
         }
     }
-
-    
-    
-    
-    
     
     createBoard()
+
+    //click on square actions
+    function click(square) {
+        if (square.classList.contains("bomb")){
+            console.log("Game over")
+        } else {
+            let total = square.getAttribute('data')
+            if(total != 0){
+                square.classList.add("checked")
+                square.innerHTML = total
+                return
+            }
+        }
+        square.classList.add("checked")
+
+    }
+
+
+
 })
